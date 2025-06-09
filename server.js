6f21db3bf163641621a94c2caba1d2a946dbca7a
Ramagiri Suraj kumar <ramagirisurajkumar@gmail.com>
2025-06-09 16:19:23 +0530

    added cors origin in server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors({
   origin: 'https://secure-in.vercel.app',
-  // origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
